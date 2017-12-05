9ab122bed66e7f5ab28a602038116a0900d5016b
NaNxT <natt.research1@gmail.com>
2017-12-05 11:26:46 +0700

    "&commit_comment& "

---
+++ macOS_script/git_command.sh
@@ -0,0 +1,46 @@
+# Author: NattN
+# Creation Date: Dec 5, 2017
+
+# push origin master
+echo -n "Do you want to proceed (y/n)? :  "
+read answer
+case ${answer:0:1} in
+    y|Y )
+        echo -n "Enter commit comment:  " 
+		read commit_comment
+		git add *
+		git commit -m ' "&commit_comment& " '
+		git push origin master
+    ;;
+    * )
+        echo Bye!
+    ;;
+esac
+
+
+
+
+# pull origin master
+echo -n "Do you want to proceed (y/n)? :  "
+read answer
+case ${answer:0:1} in
+    y|Y )
+        git push origin master
+    ;;
+    * )
+        echo Bye!
+    ;;
+esac
+
+
+
+
+# status check
+git fetch
+git status -s
+
+
+
+
+
+
